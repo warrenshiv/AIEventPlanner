de39650de1dc7fde92e7434d261836ba848abfaa
warrenshiv <warrenshiv@gmail.com>
2023-11-04 23:59:29 +0300

    Modified the code to display the event name and it's description

--- components/ResponseDisplay.js
+++ components/ResponseDisplay.js
@@ -3,20 +3,20 @@ const ResponseDisplay = ({ data, error, loading }) => {
   let content;
 
   if (loading) {
-    content = "Loading...";
+    content = "Loading..."; // Display a loading message while waiting for the AI response.
   } else if (error) {
-    content = `Error: ${error.message}`;
+    content = `Error: ${error.message}`; // Display an error message if there's an issue with the AI request.
   } else if (data) {
     console.log("Data from OpenAI API in display: ", data.result);
 
     content = (
       <>
-        <p>Name: {data.result.animalPetName}</p>
-        <p>Description: {data.result.description}</p>
+        <p>Name: {data.result.eventRecommendation}</p>
+        <p>Description: {data.result.description}</p> {/* Display the AI-generated event recommendation. */}
       </>
     );
   } else {
-    content = "";
+    content = ""; // Empty content by default.
   }
 
   return <div className="response-display">{content}</div>;
