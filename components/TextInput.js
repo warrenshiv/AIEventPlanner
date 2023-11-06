29cf0373fbb23f9ab085bbfea33fc88237d09f35
warrenshiv <warrenshiv@gmail.com>
2023-11-06 16:15:54 +0300

    Added improved handling of input clearing.

--- components/TextInput.js
+++ components/TextInput.js
@@ -1,13 +1,30 @@
-// file: /components/TextInput.js
-const TextInput = ({ value, onChange, placeholder }) => {
+import React from "react";
+
+const TextInput = ({ value, onChange, placeholder, type }) => {
+  const handleInputChange = (event) => {
+    onChange(event.target.value);
+  };
+
   return (
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-      className="text-input"
-      placeholder={placeholder}
-    />
+    <div className="text-input-container">
+      <input
+        type={type || "text"}
+        value={value}
+        onChange={handleInputChange}
+        className={`text-input ${type === "number" ? "number-input" : ""}`}
+        placeholder={placeholder}
+        required // Add the required attribute for basic validation.
+      />
+      {value && (
+        <button
+          className="clear-button"
+          onClick={() => onChange("")}
+          aria-label="Clear input"
+        >
+          &#x2715;
+        </button>
+      )}
+    </div>
   );
 };
 
